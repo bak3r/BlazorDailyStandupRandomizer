841206ae0c7010cb1dcc487e470b81a97f8549f9
Ales Tepina <ales.tepina@studio-moderna.com>
2020-11-21 19:55:04 +0100

    Basic functionality implemented

---
+++ BlazorDailyStandupRandomizer/Models/Attendee.cs
@@ -0,0 +1,8 @@
+﻿namespace BlazorDailyStandupRandomizer.Models
+{
+    public class Attendee
+    {
+        public string Name { get; set; }
+        public bool IsAttendingMeetingToday { get; set; }
+    }
+}
